eb6175670b31f3403f615f78a48ce26b6d45f4d1
anjana-cell <anjana.anju1397@gmail.com>
2026-02-25 22:56:28 +0530

    Initial commit

---
+++ geminilangchain.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+
+result = model.invoke("Explain aiagents in a few words")
+print(result.content)
---
+++ geminisdk.py
@@ -0,0 +1,12 @@
+#AI Chatbot using Gemini SDK in python
+from dotenv import load_dotenv
+load_dotenv()
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="what is n8n? explain in simple words as i am a beginner."
+)
+print(response.text)
