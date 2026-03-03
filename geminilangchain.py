848238f1845f64feaf02a4226d21f52b0409015d
anjana-cell <anjana.anju1397@gmail.com>
2026-03-03 14:48:37 +0530

    new prompt

--- geminilangchain.py
+++ geminilangchain.py
@@ -5,5 +5,5 @@ from langchain.chat_models import init_chat_model
 
 model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 
-result = model.invoke("Explain aiagents in a few words")
+result = model.invoke("what is hugging face in AI?  explain in few words")
 print(result.content)
